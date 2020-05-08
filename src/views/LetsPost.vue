ee3121d72a51c8dc353c18aea2b5bf1122a734d9
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2020-05-08 12:34:38 +0600

    lest post modified

--- src/views/LetsPost.vue
+++ src/views/LetsPost.vue
@@ -1,7 +1,9 @@
 <template>
    <div>
       <h2>Let's POST</h2>
-      
+		<p>
+			Let's post some random data to <i>jsonplaceholder.typicode.com</i>.
+		</p>
       <button @click="start = true">Start POST</button>
       <br />
       Data: {{data}} | Error: {{isError}}
@@ -11,7 +13,7 @@
          url="https://jsonplaceholder.typicode.com/posts"
          :options="{ method: 'POST',
                headers: { 'Content-Type': 'application/json' }, 
-               body: JSON.stringify({ woow: 'yahhhoo', thats: 'amazing', whatTimeIsIt: new Date() })
+               body: JSON.stringify(model)
             }"
          @resolved="_data => { data = _data; start = false; }"
       />
@@ -22,6 +24,7 @@ import Fetch from "@/components/Fetch";
 export default {
    components: { Fetch },
    data: () => ({
+		model: { woow: 'yahhhoo', thats: 'amazing', whatTimeIsIt: new Date() },
       start: false,
       data: null,
       isError: false
